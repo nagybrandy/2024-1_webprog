4a03089c2239d391ff647841702ba6849d8ef03b
nagybrandy <57422699+nagybrandy@users.noreply.github.com>
2024-03-12 13:39:23 +0100

    03.12

--- 4. csoport/05. gyakorlat/hello-world/src/App.jsx
+++ 4. csoport/05. gyakorlat/hello-world/src/App.jsx
@@ -5,27 +5,6 @@ import CardContainer from './CardContainer'
 const App = () => {
     return (
         <div className='w-[80%] mx-auto'> {/* fragment */}
-        <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
-      <li>
-        <details>
-          <summary>
-            Parent
-          </summary>
-          <ul className="p-2 bg-base-100 rounded-t-none">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
-          </ul>
-        </details>
-      </li>
-    </ul>
-  </div>
-</div>
             <h1 className='text-primary text-4xl mb-10 mt-5'>Emberek</h1>
             <Hello name="Gazsi" age={31} emoji="👨‍🚒"/>
             <Hello name="Jakab" age={16} emoji="🥔"/>
@@ -36,3 +15,4 @@ const App = () => {
 }
 
 export default App
+
